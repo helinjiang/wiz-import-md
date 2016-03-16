3fb0214325a738cb8400204d284840718180fd98
helinjiang <me@helinjiang.com>
2016-03-16 14:38:30 +0800

    增加注释

--- Import.Md/resource/app.js
+++ Import.Md/resource/app.js
@@ -129,30 +129,42 @@ function getLocalString(filename, stringName) {
     return objApp.LoadStringFromFile(filename, stringName);
 }
 
-function DoImport(path) {
+/**
+ * 导入指定路径中的文件
+ * @param  {string}   path 指定路径
+ */
+function doImport(path) {
+    // objApp.AppPath wiz的安装路径
     var templateFileName = objApp.AppPath + "templates\\new\\journal\\journal.template.htm";
+    
+    // 从一个text文件获得文字内容。可以有效地获得避免乱码。bstrFileName：文件名；返回值：text里面的文字
     var templateHtml = objCommon.LoadTextFromFile(templateFileName);
+    
     var journalIniFileName = objApp.AppPath + "templates\\new\\journal.ini";
 
-    //
+    ///枚举一个文件夹下面的文件。bstrPath：文件夹路径；bstrFileExt：扩展名，格式为*.txt;*.jpg;*.png或者*.*；vbIncludeSubFolders：是否包含子文件夹；
+    ///返回值：枚举到的文件名。类型为一个安全数组，如果在javascript里面使用，请参阅本文后面部分。
     var objFiles = objCommon.EnumFiles(path, "*.html", false);
+   
     //
     var arrFiles = objFiles;
 
-    var asjournal = buttonJournal.checked;
-    //
+    // 进度条
     var objProgress = objApp.CreateWizObject("WizKMControls.WizProgressWindow");
     objProgress.Show();
-    //
     objProgress.Max = arrFiles.length;
     objProgress.Title = "正在导入...";
-    //
+
+    // 遍历文件
     for (var iFile = 0; iFile < arrFiles.length; iFile++) {
         var filename = arrFiles[iFile];
-        //
+
+        // 进度条上显示当前处理的文件名
         objProgress.Text = filename;
-        //
+
+        // 获得文件内容
         var html = objCommon.LoadTextFromFile(filename);
+
         //
         var objParts = objCommon.HtmlExtractTags(html, "div", "class", "qqshowbd");
         if (null == objParts)
@@ -225,11 +237,20 @@ function DoImport(path) {
             } else {
                 htmltext = parthtml;
             }
-            //
+
+            // 更改文档标题和文件名
             objDoc.ChangeTitleAndFileName(title);
-            //
+
+            /**
+             * http://www.wiz.cn/manual/plugin/api/descriptions/IWizDocument.html
+             * 
+             * 更改文档数据，通过HTML文字内容来更新
+             * [id(52), helpstring("method UpdateDocument3")] HRESULT UpdateDocument3([in] BSTR bstrHtml, [in] LONG nFlags);
+             */
             objDoc.UpdateDocument3(htmltext, 0);
         }
+
+        // 进度条进度移动到下一个为知
         objProgress.Pos = iFile + 1;
     }
 }
@@ -251,7 +272,7 @@ function submitDialog() {
     }
 
     // 导入文件到选中的路径
-    DoImport(basepath);
+    doImport(basepath);
 
     // 关闭对话框
     closeDialog();
@@ -269,6 +290,8 @@ function closeDialog() {
  */
 function clickToBrowse() {
     /**
+     * http://www.wiz.cn/manual/plugin/api/descriptions/IWizCommonUI.html
+     * 
      * 提示用户选择一个磁盘文件夹。bstrDescription：描述；返回值：用户选择的文件夹
      * [id(16), helpstring("method SelectWindowsFolder")] HRESULT SelectWindowsFolder([in] BSTR bstrDescription, [out,retval] BSTR* pbtrFolderPath);
      */
